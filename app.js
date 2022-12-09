3c1f969fada6971389e5fb906354a79e56aa2976
Elthon <eltonsilva.mecatronic@gmail.com>
2022-12-08 22:19:27 -0300

    alteracoes

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Ok, Deu Certo'
-    });
-});
+const rotaProdutos = require('./routes/produtos');
+
+app.use('/produtos', rotaProdutos);
 
 module.exports = app;
