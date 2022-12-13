57a9e0890d8b06531a094a4a0bc7b3d8a83a03ec
Elthon <eltonsilva.mecatronic@gmail.com>
2022-12-12 22:39:13 -0300

    alteracoes no produto

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+//RETORNA TODOS OS PEDIDOS
 router.get('/', (req, res, next) => {
     res.status(200).send({
         mensagem: 'Usando o GET dentro da rota de produtos'
     });
 });
 
+//INSERE UM PRODUTO
 router.post('/', (req, res, next) => {
     res.status(201).send({
         mensagem: 'Usando o POST dentro da rota de produtos'
     });
 });
 
+//RETORNA OS DADOS DE UM PRODUTO
 router.get('/:id_produto', (req, res, next) => {
     const id = req.params.id_produto
 
@@ -26,6 +29,20 @@ router.get('/:id_produto', (req, res, next) => {
             mensagem: 'Você passou um Id'
         })
     }
+});
+
+//ALTERA UM PRODUTO
+router.patch('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o PATCH dentro da rota de produtos'
+    })
+});
+
+//EXCLUI UM PRODUTO
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o DELETE dentro da rota de produtos'
+    })
 });
 
 module.exports = router;
