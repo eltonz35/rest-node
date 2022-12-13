30a6e9816ca9cfffa4ed7e130b9eac70e5c6fa09
Elthon <eltonsilva.mecatronic@gmail.com>
2022-12-12 22:38:37 -0300

    tratamento de erros

--- app.js
+++ app.js
@@ -5,4 +5,20 @@ const rotaProdutos = require('./routes/produtos');
 
 app.use('/produtos', rotaProdutos);
 
+
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    })
+});
+
 module.exports = app;
