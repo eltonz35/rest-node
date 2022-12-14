de2c43adb3829982a28087cd228b6675cdf4ec40
Elthon <eltonsilva.mecatronic@gmail.com>
2022-12-13 22:12:59 -0300

    fazendo ajustes

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const rotaProdutos = require('./routes/produtos');
 
 app.use('/produtos', rotaProdutos);
 
+
+app.use(bodyParser.urlencoded({ extended: false})); // apenas dados simples
+app.use(bodyParser.json()); // json na entrada do body  
 
 app.use((req, res, next) => {
     const erro = new Error('Não encontrado');
